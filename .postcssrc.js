af4a6fa77410aab5f35c314b45cf610ad149f007
clannadx <xxi124@sina.com>
2019-09-18 10:03:07 +0800

    web

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,30 +1,30 @@
-const autoprefixer = require('autoprefixer')
-const pxtorem = require('postcss-pxtorem')
+// const autoprefixer = require('autoprefixer')
+// const pxtorem = require('postcss-pxtorem')
 
-module.exports = ({ file }) => {
-  let rootValue
-  // vant 37.5 [link](https://github.com/youzan/vant/issues/1181)
-  if (file && file.dirname && file.dirname.indexOf('vant') > -1) {
-    rootValue = 37.5
-  } else {
-    rootValue = 37.5
-  }
-  return {
-    plugins: [
-      autoprefixer(),
-      pxtorem({
-        rootValue: rootValue,
-        propList: ['*'],
-        // unitPrecision:10,
-        // selectorBlackList:[/^body$/],
-        minPixelValue: 2
-      })
-    ]
-  }
-}
-
-// module.exports = {
-//   plugins: {
-//     autoprefixer: {}
+// module.exports = ({ file }) => {
+//   let rootValue
+//   // vant 37.5 [link](https://github.com/youzan/vant/issues/1181)
+//   if (file && file.dirname && file.dirname.indexOf('vant') > -1) {
+//     rootValue = 37.5
+//   } else {
+//     rootValue = 37.5
 //   }
-// };
+//   return {
+//     plugins: [
+//       autoprefixer(),
+//       pxtorem({
+//         rootValue: rootValue,
+//         propList: ['*'],
+//         // unitPrecision:10,
+//         // selectorBlackList:[/^body$/],
+//         minPixelValue: 2
+//       })
+//     ]
+//   }
+// }
+
+module.exports = {
+  plugins: {
+    autoprefixer: {}
+  }
+};
